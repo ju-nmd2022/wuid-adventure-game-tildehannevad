0ada0ae4b410447de1f2660e9fd10ef22c6a4bb0
tildehannevad <hati22fq@student.ju.se>
2022-11-23 12:31:33 +0100

    bone, collar and key

--- main.js
+++ main.js
@@ -7,41 +7,71 @@ const backyard = document.getElementById("canvas");
 const arrowLeft = document.getElementById("arrowLeft");
 const arrowRight = document.getElementById("arrowRight");
 const key = document.getElementById("fenceKey");
+const inventoryKey = document.getElementById("inventoryKey");
+const inventoryDogbone = document.getElementById("inventoryDogbone");
+const inventoryDogCollar = document.getElementById("inventoryDogCollar");
+const errorMessage = document.getElementById("guide");
+const dogbone = document.getElementById("parkDogbone");
+const dogCollar = document.getElementById("parkDogCollar");
 
 let accessKey = false;
-
+let hasKey = false;
+let hasDogCollar = false;
+let hasDogbone = false;
 let currentScene = 1;
 let sceneCount = 0;
+inventoryKey.style.filter = "opacity(0.25)";
+inventoryDogbone.style.filter = "opacity(0.25)";
+inventoryDogCollar.style.filter = "opacity(0.25)";
 chanceScene(currentScene);
+
+hideErrorMessage();
 
 function chanceScene(scene) {
   let imageSource;
-  hideKey();
+  hideErrorMessage();
+  hideDogbone();
+  hideDogCollar();
   if (scene == 1) {
+    showKey();
     currentScene = 1;
     imageSource = FIRST;
-    sceneCount += 1;
-    if (sceneCount > 1) {
-      showKey();
-    }
+    // sceneCount += 1;
   } else if (scene == 2) {
-    currentScene = 2;
-    imageSource = SECOND;
+    if (hasKey) {
+      hideKey();
+      hideErrorMessage();
+      currentScene = 2;
+      imageSource = SECOND;
+    } else {
+      showErrorMessage(
+        "You must take the key with you before continuing to the next page!"
+      );
+    }
   } else if (scene == 3) {
-    currentScene = 3;
-    imageSource = THIRD;
-    sceneCount += 1;
-    if (sceneCount > 1) {
-      showKey();
+    hideErrorMessage();
+    if (hasDogCollar) {
+      hideDogCollar();
+      hideErrorMessage();
+      currentScene = 2;
+      imageSource = SECOND;
+    } else {
+      showErrorMessage(
+        "You must take the dogcollar and dogbone with you before continuing to the next page!"
+      );
+      showDogbone();
+      showDogCollar();
+      currentScene = 3;
+      imageSource = THIRD;
+      sceneCount += 1;
     }
   } else if (scene == 4) {
     currentScene = 4;
     imageSource = FORTH;
     sceneCount += 1;
-    if (sceneCount > 1) {
-      showKey();
-    }
   }
+
+  console.log(arrowRight);
 
   if (imageSource != null) {
     backyard.src = imageSource;
@@ -51,7 +81,7 @@ function chanceScene(scene) {
 arrowLeft.addEventListener("click", function () {
   let nextPage = currentScene - 1;
   if (nextPage <= 0) {
-    nextPage = 4;
+    nextPage = 0;
   }
   chanceScene(nextPage);
 });
@@ -64,10 +94,77 @@ arrowRight.addEventListener("click", function () {
   chanceScene(nextPage);
 });
 
+key.addEventListener("click", function () {
+  inventoryKey.style.filter = "";
+  if (!hasKey) {
+    hasKey = true;
+    hideKey();
+  }
+});
+
+dogbone.addEventListener("click", function () {
+  inventoryDogbone.style.filter = "";
+  if (!hasDogbone) {
+    hasDogbone = true;
+    hideDogbone();
+  }
+});
+
+dogCollar.addEventListener("click", function () {
+  inventoryDogCollar.style.filter = "";
+  if (!hasDogCollar) {
+    hasDogCollar = true;
+    hideDogCollar();
+  }
+});
+
 function showKey() {
   key.hidden = false;
 }
 
 function hideKey() {
   key.hidden = true;
+}
+
+function hideDogbone() {
+  dogbone.hidden = true;
+}
+
+function showDogbone() {
+  dogbone.hidden = false;
+}
+
+function hideDogCollar() {
+  dogCollar.hidden = true;
+}
+
+function showDogCollar() {
+  dogCollar.hidden = false;
+}
+
+function hideErrorMessage() {
+  errorMessage.style.display = "none";
+}
+
+function showErrorMessage(message) {
+  errorMessage.style.display = "flex";
+  errorMessage.innerHTML = message;
+}
+
+function showKey() {
+  if (!hasKey) {
+    key.hidden = false;
+  }
+}
+
+function showDogCollar() {
+  if (!hasDogCollar) {
+    dogCollar.hidden = false;
+  }
+}
+
+function showDogbone() {
+  if (!hasDogbone) {
+    dogbone.hidden = false;
+  }
 }
